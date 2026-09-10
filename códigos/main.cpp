310029bbc06390b394e5027372db7c1efd5892cb
unknown <lucasguerraprocopio25@gmail.com>
2026-09-10 09:26:21 -0300

    fiz jogos pagos

--- códigos/main.cpp
+++ códigos/main.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
 #include <string>
 
-class jogos{
+class jogos_gratuitos{
     private:
         std::string titulo;
-        double valor;
         double tamanho;
         int horas_jogadas = 0;
 
     public:
-        jogos(std::string nome,double preco, double peso){
+        jogos_gratuitos(std::string nome, double peso){
             this->titulo = nome;
-            this->valor = preco;
             this->tamanho = peso;
         };
 
-        ~jogos(){
+        ~jogos_gratuitos(){
             std::cout << "O jogo: " << titulo << " foi desinstalado." << std::endl;
         };
 
         void SetTitulo(std::string nome){
             this->titulo = nome;
-        };
-
-        void SetValor(double preco){
-            this->valor = preco;
         };
 
         void SetTamanho(double peso){
@@ -39,10 +33,6 @@ class jogos{
             return titulo;
         };
 
-        double GetValor(){
-            return valor;
-        };
-
         double GetTamanho(){
             return tamanho;
         };
@@ -51,16 +41,39 @@ class jogos{
             return horas_jogadas;
         };
 
-        void Displaay(){
-            std::cout << "O jogo: " << titulo <<" custa: " << valor << " reais, e pesa: " << tamanho << " Gigabytes\n" << "Suas horas jogadas: " << horas_jogadas << std::endl;
+        void Display(){
+            std::cout << "O jogo: " << titulo << "pesa: " << tamanho << " Gigabytes\n" << "Suas horas jogadas: " << horas_jogadas << std::endl;
         };
+
+    };
+
+    class jogo_pagos : public jogos_gratuitos {
+        private:
+            int valor;
+            bool comprado;
+
+
+        public:
+
+            jogo_pagos(std::string nome, double peso, int preco, bool adiquirido) : jogos_gratuitos(nome, peso){
+                this->valor = preco;
+                this->comprado = adiquirido;
+            };
+
+
 
     };
 
 int main(){
 
-    jogos Fortinite("Fortinite", 0, 90);
-
+    jogos_gratuitos Fortinite("Fortinite", 90);
+    jogos_gratuitos R6("Rainbow six siege", 75);
+    jogos_gratuitos Minecraft("Minecraft", 35);
+    jogos_gratuitos MK("Mortal Kombat", 80);
+    jogos_gratuitos Fifa("FC 26", 80);
+    jogos_gratuitos Roblox("Roblox", 5);
+    jogos_gratuitos FNAF("Five nights at Freddy", 10);
+    jogos_gratuitos Gta("Grand Theft Auto 6", 220);
 }
 
 
